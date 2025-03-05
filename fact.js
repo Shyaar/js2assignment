4c80002b9fff63fe852b1ba95dde0fe8863686d7
Shyaar Sati Fwatshak <shyaarsati@gmial.com>
2025-03-05 15:04:32 +0100

    first commit

---
+++ fact.js
@@ -0,0 +1,21 @@
+// Calculating Factorial Using While Loop in JavaScript
+
+let r = [1,2,3]
+let product = r.product((acc,num)=>{acc*num,1})
+console.log(product)
+
+console.log(r)
+
+let n = 10
+
+
+// while(n>0){
+//     let factorial =[]
+//     console.log(n)
+//     if n
+//     let cal=(n-1)
+//     factorial.push(n*cal)
+//     n--
+
+//     console.log(factorial)
+// }
